06cea9072598cb4c4749745f8372214d6c511b05
Jon Suda <jon@synnduit.com>
2022-02-24 19:31:17 -0700

    Conversion from the .NET Framework version

---
+++ src/Synnduit.TestHelper/ArgumentTester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+
+namespace Synnduit.TestHelper
+{
+    public static class ArgumentTester
+    {
+        public static void ThrowsArgumentException(
+            Action invokeTestedMethod, string expectedParamName)
+        {
+            ThrowsArgumentException<ArgumentException>(
+                invokeTestedMethod, expectedParamName);
+        }
+
+        public static void ThrowsArgumentNullException(
+            Action invokeTestedMethod, string expectedParamName)
+        {
+            ThrowsArgumentException<ArgumentNullException>(
+                invokeTestedMethod, expectedParamName);
+        }
+
+        private static void ThrowsArgumentException<TException>(
+            Action invokeTestedMethod, string expectedParamName)
+            where TException : ArgumentException
+        {
+            // arrange
+            TException exceptionThrown = null;
+
+            // act
+            try
+            {
+                invokeTestedMethod();
+            }
+            catch(TException exception)
+            {
+                exceptionThrown = exception;
+            }
+
+            // assert
+            exceptionThrown.Should().NotBeNull();
+            exceptionThrown.ParamName.Should().Be(expectedParamName);
+        }
+    }
+}
